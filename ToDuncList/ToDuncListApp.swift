78097a8876e1bf1b279210eba339cd90efc59090
Kuncans <akafauna@gmail.com>
2022-01-23 13:13:41 +0000

    Initial Commit

---
+++ ToDuncList/ToDuncListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDuncListApp.swift
+//  ToDuncList
+//
+//  Created by Duncan Kent on 23/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDuncListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
