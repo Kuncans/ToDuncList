86f27216fdf89203aff5003244bb26646d7235e1
Kuncans <akafauna@gmail.com>
2022-01-26 23:42:29 +0000

    created listviewmodel and moved logic out of listview

--- ToDuncList/View/ListView.swift
+++ ToDuncList/View/ListView.swift
@@ -9,33 +9,22 @@ import SwiftUI
 
 struct ListView: View {
     
-    @State private var toDuncs: [ToDunc] = [
-        ToDunc(title: "Get out of bed", isCompleted: false),
-        ToDunc(title: "Eat 142 pineapples", isCompleted: true),
-        ToDunc(title: "Win village simulator 2048", isCompleted:false)]
+    @EnvironmentObject var listViewModel: ListViewModel
     
     var body: some View {
         
         List {
-            ForEach(toDuncs) { toDunc in
+            ForEach(listViewModel.toDuncs) { toDunc in
                 ToDuncCell(item: toDunc)
             }
-            .onDelete(perform: deleteItem)
-            .onMove(perform: moveItem)
+            .onDelete(perform: listViewModel.deleteItem)
+            .onMove(perform: listViewModel.moveItem)
         }
         .listStyle(PlainListStyle())
         .navigationTitle("To Dunc Items ✔")
         .navigationBarItems(leading: EditButton(),
                             trailing: NavigationLink("Add", destination: AddView())
         )
-    }
-    
-    func deleteItem(indexSet: IndexSet) {
-        toDuncs.remove(atOffsets: indexSet)
-    }
-    
-    func moveItem(from: IndexSet, to: Int) {
-        toDuncs.move(fromOffsets: from, toOffset: to)
     }
     
 }
@@ -45,6 +34,7 @@ struct ListView_Previews: PreviewProvider {
         NavigationView {
             ListView()
         }
+        .environmentObject(ListViewModel())
     }
 }
 
---
+++ ToDuncList/ViewModel/ListViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  ListViewModel.swift
+//  ToDuncList
+//
+//  Created by Duncan Kent on 26/01/2022.
+//
+
+import Foundation
+
+final class ListViewModel: ObservableObject {
+    
+    @Published var toDuncs: [ToDunc] = []
+    
+    init() {
+        getItems()
+    }
+    
+    func getItems () {
+        let newDuncs = [
+            ToDunc(title: "Get out of bed", isCompleted: false),
+            ToDunc(title: "Eat 142 pineapples", isCompleted: true),
+            ToDunc(title: "Win village simulator 2048", isCompleted:false)
+        ]
+        toDuncs.append(contentsOf: newDuncs)
+    }
+    
+    func deleteItem(indexSet: IndexSet) {
+        toDuncs.remove(atOffsets: indexSet)
+    }
+    
+    func moveItem(from: IndexSet, to: Int) {
+        toDuncs.move(fromOffsets: from, toOffset: to)
+    }
+    
+    
+}
